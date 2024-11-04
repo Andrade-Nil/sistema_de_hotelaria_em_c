c4685f546ae47892eb9988c591dad912805ec25b
Anderson <anderson@agenciaw3.digital>
2024-11-04 15:10:05 -0400

    "Updated hospedes.txt and locacoes.txt files, added new function declarations to locacao.h"

--- include/locacao.h
+++ include/locacao.h
@@ -18,8 +18,12 @@ void adicionar_locacao(Locacao nova_locacao);
 void atualizar_locacao(int id, Locacao locacao_atualizada);
 void excluir_locacao(int id);
 void salvar_locacoes();
-void listar_locacoes(); // Função para listar locações
+void listar_locacoes();
 void gerenciar_locacoes();
+
+// Função para verificar se o quarto está disponível
+int quarto_disponivel(int id_quarto, const char* data_inicial, int diarias);
+int datas_conflitantes(const char* data1, int diarias1, const char* data2, int diarias2); // Verifica se duas locações conflitam
 
 extern Locacao locacoes[MAX_LOCACOES]; // Declare a variável externa aqui
 extern int contador_locacoes; // Declare a variável externa aqui
